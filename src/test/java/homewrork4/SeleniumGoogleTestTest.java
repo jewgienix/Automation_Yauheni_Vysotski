d5efdc9023fe198eab631ab47ae897c032484503
Yauheni Vysotski <yauhenivysotski@gmail.com>
2023-11-26 15:08:12 +0100

    Homework4

---
+++ src/test/java/homewrork4/SeleniumGoogleTestTest.java
@@ -0,0 +1,48 @@
+package homewrork4;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class SeleniumGoogleTestTest {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    @Test
+    public void seleniumGoogleTest() {
+        driver.get("https://www.google.by/");
+        driver.manage().window().setSize(new Dimension(1470, 849));
+        driver.findElement(By.id("APjFqb")).sendKeys("he");
+        driver.findElement(By.cssSelector(".sbhl .pcTkSc")).click();
+        driver.findElement(By.cssSelector("#rso > .MjjYud:nth-child(1) .LC20lb")).click();
+    }
+}
